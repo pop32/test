8ed14ab475d12c0193d44a8bd92bb230ad17d42f
lain137 <higurashi07exp@gmail.com>
2018-12-31 00:11:32 +0900

    save

--- src/readme.md
+++ src/readme.md
@@ -8,6 +8,7 @@ memo
 pixijs チュートリアル
 https://github.com/kittykatattack/learningPixi
 
+https://pixijs.io/examples/#/basics/basic.js
 
 Re:ゼロから始めるElectron開発生活
 https://qiita.com/nagisio/items/259bef1748a4db8e0af6
---
+++ src/test/12JSON作成.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+import datetime
+import sqlite3
+from contextlib import closing
+
+def strtodt(s):
+    d=datetime.datetime(\
+        int(s[0:4]),\
+        int(s[4:6]),\
+        int(s[6:8]),\
+        int(s[8:10]),\
+        int(s[10:12]),\
+        int(s[12:14]))
+    return d
+
+with closing(sqlite3.connect("data/gbpjpy_tick_60min.db")) as conn:
+    sql = "select * from tick where open <> -1 order by dt desc"
+    row = conn.execute(sql)
+    print("var testdata=[")
+    for r in row:
+        dt=strtodt(r[0])
+        s='{dt:"' + dt.strftime('%Y/%m/%d %H:%M:%S')  + '"'\
+          +',open:' + str(r[1])\
+          +',end:' + str(r[2])\
+          +',high:' + str(r[3])\
+          +',low:' + str(r[4])\
+          +'},'
+
+        print(s)
+    print("];")
